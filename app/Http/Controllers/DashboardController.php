f5839e7c7c9625dc0adec3ff0c695cd2b639fad5
ekasatriaap <ekasatria.ariaputra@gmail.com>
2025-03-09 18:49:00 +0000

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TaskHd;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,9 +11,19 @@ class DashboardController extends Controller
 
     public function index()
     {
+        $id_user = auth()->user()->id;
+        $tugas = TaskHd::where("id_penerima_tugas", $id_user)->get();
+        $penugasan = TaskHd::where("id_pemberi_tugas", $id_user)->get();
         $data = [
             "title" => "Dashboard",
-            "work_order_pending" => 100
+            "tugas_pending" => $tugas->where("status", TASK_STATUS_PENDING)->count(),
+            "tugas_in_progress" => $tugas->where("status", TASK_STATUS_IN_PROGRESS)->count(),
+            "tugas_completed" => $tugas->where("status", TASK_STATUS_COMPLETED)->count(),
+            "tugas_canceled" => $tugas->where("status", TASK_STATUS_CANCELED)->count(),
+            "penugasan_pending" => $penugasan->where("status", TASK_STATUS_PENDING)->count(),
+            "penugasan_in_progress" => $penugasan->where("status", TASK_STATUS_IN_PROGRESS)->count(),
+            "penugasan_completed" => $penugasan->where("status", TASK_STATUS_COMPLETED)->count(),
+            "penugasan_canceled" => $penugasan->where("status", TASK_STATUS_CANCELED)->count(),
         ];
         return view($this->view, $data);
     }
--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -1,6 +1,24 @@
 <x-app-layout>
     <x-row>
-        <x-dashboard-card class="col-lg-6" title="Total Tugas Pending" :value="$work_order_pending" color="light"
-            icon="fas fa-file-alt" />
+        @can('tugas.lihat')
+            <x-dashboard-card class="col-lg-6" title="Total Tugas Pending" :value="$tugas_pending" color="secondary"
+                icon="fas fa-hourglass-half" />
+            <x-dashboard-card class="col-lg-6" title="Total Tugas In Progress" :value="$tugas_in_progress" color="info"
+                icon="fas fa-spinner" />
+            <x-dashboard-card class="col-lg-6" title="Total Tugas Completed" :value="$tugas_completed" color="success"
+                icon="fas fa-check" />
+            <x-dashboard-card class="col-lg-6" title="Total Tugas Canceled" :value="$tugas_canceled" color="danger"
+                icon="fas fa-times" />
+        @endcan
+        @can('penugasan.lihat')
+            <x-dashboard-card class="col-lg-6" title="Total Penugasan Pending" :value="$penugasan_pending" color="secondary"
+                icon="fas fa-hourglass-half" />
+            <x-dashboard-card class="col-lg-6" title="Total Penugasan In Progress" :value="$penugasan_in_progress" color="info"
+                icon="fas fa-spinner" />
+            <x-dashboard-card class="col-lg-6" title="Total Penugasan Completed" :value="$penugasan_completed" color="success"
+                icon="fas fa-check" />
+            <x-dashboard-card class="col-lg-6" title="Total Penugasan Canceled" :value="$penugasan_canceled" color="danger"
+                icon="fas fa-times" />
+        @endcan
     </x-row>
 </x-app-layout>
